1685e3e46253dc33954bfa40d31c039d7478daf1
Marcin Dylewski <marcin.dylewski@silksh.pl>
2023-01-02 15:49:54 +0100

    ssss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,25 +6,25 @@
         <div class="px-4 py-16 mx-auto sm:max-w-xl text-white md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div class="grid grid-cols-2 row-gap-8 md:grid-cols-4">
                 <div class="text-center md:border-r">
-                    <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl" style="color: #ac22ec">{{ $players }}</h6>
+                    <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">{{ $players }}</h6>
                     <p class="text-sm font-medium tracking-widest uppercase lg:text-base" style="color: #22a0ec">
                         Graczy
                     </p>
                 </div>
                 <div class="text-center md:border-r">
-                    <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl" style="color: #ac22ec">4</h6>
+                    <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">4</h6>
                     <p class="text-sm font-medium tracking-widest uppercase lg:text-base" style="color: #22a0ec">
                         Moderatorów
                     </p>
                 </div>
                 <div class="text-center md:border-r">
-                    <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl" style="color: #ac22ec">{{ round(($allKills / 1000), 1) }}K</h6>
+                    <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">{{ round(($allKills / 1000), 1) }}K</h6>
                     <p class="text-sm font-medium tracking-widest uppercase lg:text-base" style="color: #22a0ec">
                         Zabójstw
                     </p>
                 </div>
                 <div class="text-center">
-                    <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl" style="color: #ac22ec">{{ round(($allWins / 1000), 1) }}K</h6>
+                    <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">{{ round(($allWins / 1000), 1) }}K</h6>
                     <p class="text-sm font-medium tracking-widest uppercase lg:text-base" style="color: #22a0ec">
                         Wygranych Meczy
                     </p>
