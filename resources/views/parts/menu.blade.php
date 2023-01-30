ce84d1713c6d53b0f60b99f44c32b52a526121d9
Marcin1sto <marcin1sto@gmail.com>
2023-01-30 11:04:11 +0100

    change logo

--- resources/views/parts/menu.blade.php
+++ resources/views/parts/menu.blade.php
@@ -1,9 +1,8 @@
 <nav class="flex justify-center px-2 sm:px-4 py-2.5 bg-gray-800 sticky w-full z-20 top-0 left-0 border-b border-green-500" style="height: 100px">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
         <a href="/" class="flex items-center">
-            <h1 class="px-5 font-medium text-3xl text-white">PUBG <span class="font-bold text-2xl">Polska</span></h1>
-{{--                        <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">--}}
-{{--                        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>--}}
+            <img src="images/logo.png" class="mr-3 h-14" alt="PUBG polska Logo">
+
         </a>
         <div class="flex md:order-2">
             <div class="hidden lg:block">
