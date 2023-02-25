e52f769daff1659d2e2bc4aa40bf8bfe8689eb30
Marcin Dylewski <marcin.dylewski@silksh.pl>
2023-02-25 17:47:59 +0100

    hot fix

--- app/Services/RankingService.php
+++ app/Services/RankingService.php
@@ -113,10 +113,8 @@ class RankingService
     {
         $player = Player::with('discordRanking')->where('playerName', $playerName)->first();
 
-        dd($player->discordRanking->where('type', $matchMode)->first());
-
         if ($player && $player->discordRanking) {
-            return $player->discordRanking;
+            return $player->discordRanking->where('type', $matchMode)->first();
         }
 
         return null;
