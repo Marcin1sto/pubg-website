d8195de95297a1ca44de648505a8f7306d703f7c
Juraso2 <juraso97@gmail.com>
2025-02-14 22:08:21 +0100

    Docker container config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,30 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: {enabled: true},
-  css: ['@@/assets/css/tailwind.css'],
-  options: {
-    ssr: true,
-    target: 'static',
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    css: ['@@/assets/css/tailwind.css'],
+    options: {
+        ssr: true,
+        target: 'static',
+        postcss: {
+            plugins: {
+                tailwindcss: {},
+                autoprefixer: {},
+            },
+        },
     },
-  },
-  tailwindcss: {
-    configPath: "./tailwind.config.js",
-    exposeConfig:true,
-    viewer: true,
-  },
-  modules: [
-    // '@pinia/nuxt',
-    // '@nuxtjs/i18n',
-    // '@nuxt/image',
-    // '@pinia-plugin-persistedstate/nuxt',
-    '@nuxtjs/tailwindcss',
-    // '@tailwindcss/postcss'
-  ],
+    tailwindcss: {
+        configPath: "./tailwind.config.js",
+        exposeConfig: true,
+        viewer: true,
+    },
+    modules: [
+        // '@pinia/nuxt',
+        // '@nuxtjs/i18n',
+        // '@nuxt/image',
+        // '@pinia-plugin-persistedstate/nuxt',
+        '@nuxtjs/tailwindcss',
+        // '@tailwindcss/postcss'
+    ],
+    $development: {
+        vite: {
+            server: {
+                watch: {
+                    usePolling: true,
+                },
+                hmr: {
+                    protocol: "ws",
+                    host: "0.0.0.0",
+                    port: 24678,
+                },
+            },
+        },
+    },
 })
